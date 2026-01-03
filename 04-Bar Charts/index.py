bbaf9a2c23e36a3c139dcd00e75e7adcfe6ef7f1
CodeQueen-AI <codeq209@gmail.com>
2026-01-03 23:18:47 +0500

    update

--- 04-Bar Charts/index.py
+++ 04-Bar Charts/index.py
@@ -1,33 +1,34 @@
 import matplotlib.pyplot as plt
 
 # Vertical Bar
-# categories = ['A' , 'B' , 'C' , 'D']
-# values = [10, 15, 7, 12]
-
-# plt.bar(categories, values)
-
-# plt.title('Category values')
-# plt.xlabel('Categories')
-# plt.ylabel('Values')
-
-# plt.show()
-
-# # Horizontal Bar
-# plt.bar(categories, values)
-
-# Grouped Bar Chart
 categories = ['A' , 'B' , 'C' , 'D']
+values = [10, 15, 7, 12]
+
+plt.bar(categories, values)
+
+plt.title('Category values')
+plt.xlabel('Categories')
+plt.ylabel('Values')
+
+plt.show()
+
+# Horizontal Bar
+plt.bar(categories, values)
+
+# Grouped bar Chart
+categories = ["A", "B", "C", "D"]
 values1 = [10, 15, 7, 12]
 values2 = [8, 12, 5, 10]
 
-plt.bar(categories, values1, width=0.4, label='2025' , align='edge', color='green')
-plt.bar(categories, values1, width=0.4, label='2026' , align='center', color='orange')
+plt.bar(categories, values1, width=0.4, label="2025", align="edge", color="skyblue")
+plt.bar(categories, values2, width=0.4, label="2026", align="center", color="orange")
 
-plt.title('Category Comparison')
-plt.xlabel('Categories')
-plt.label('Values')
+plt.title("Category Comparison")
+plt.xlabel("Categories")
+plt.ylabel("Values")
 plt.legend()
 plt.show()
+
 
 
 
