d8e5070da195f21d79b00df8bb7c6bb1737ce3d7
CodeQueen-AI <codeq209@gmail.com>
2026-01-04 13:54:19 +0500

    update

--- 01-Line Plot/index.py
+++ 01-Line Plot/index.py
@@ -1,11 +1,32 @@
+# import matplotlib.pyplot as plt
+
+# x = [1,2,3,4,5]
+# y = [10,0,15,25,30]
+
+# plt.plot(x , y)
+# plt.title('Simple Line Plot')
+# plt.xlabel('X Axis')
+# plt.ylabel('Y Axis')
+
+# plt.show()
+
+
+
+
+
+
 import matplotlib.pyplot as plt
 
-x = [1,2,3,4,5]
-y = [10,0,15,25,30]
+x = [1, 2, 3, 4, 5]
+y = [10, 0, 15, 25, 30]
 
-plt.plot(x , y)
-plt.title('Simple Line Plot')
-plt.xlabel('X Axis')
-plt.ylabel('Y Axis')
+plt.plot(x, y, label="Data Line")   # legend ke liye label
+
+plt.title("Simple Line Plot")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+plt.legend()        # legend
+plt.grid(True)     # grid
 
 plt.show()
--- 06-Scatter Plot/index.py
+++ 06-Scatter Plot/index.py
@@ -1,18 +1,3 @@
-# import matplotlib.pyplot as plt
-
-# x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11]
-# y = [99, 86, 87, 88, 100, 86, 103, 87, 94, 78]
-
-# plt.scatter(x, y)
-
-# plt.title('Simple Scatter Plot')
-# plt.xlabel('X Values')
-# plt.ylabel('Y Values')
-# plt.show()
-
-
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
