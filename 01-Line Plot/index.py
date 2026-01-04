71fd69e16a61c5a58f0b6bd371b5a973d24ac55b
CodeQueen-AI <codeq209@gmail.com>
2026-01-04 13:58:41 +0500

    sa

--- 01-Line Plot/index.py
+++ 01-Line Plot/index.py
@@ -1,32 +1,15 @@
-# import matplotlib.pyplot as plt
-
-# x = [1,2,3,4,5]
-# y = [10,0,15,25,30]
-
-# plt.plot(x , y)
-# plt.title('Simple Line Plot')
-# plt.xlabel('X Axis')
-# plt.ylabel('Y Axis')
-
-# plt.show()
-
-
-
-
-
-
 import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5]
 y = [10, 0, 15, 25, 30]
 
-plt.plot(x, y, label="Data Line")   # legend ke liye label
+plt.plot(x, y, label="Data Line")   
 
 plt.title("Simple Line Plot")
 plt.xlabel("X Axis")
 plt.ylabel("Y Axis")
 
-plt.legend()        # legend
-plt.grid(True)     # grid
+plt.legend()       
+plt.grid(True)   
 
 plt.show()
--- 17-Step Plot/index.py
+++ 17-Step Plot/index.py
@@ -4,7 +4,17 @@ x = [1, 2, 3, 4, 5]
 y = [10, 15, 12, 18, 20]
 
 plt.step(x, y)
-plt.title("Simple Step Plot")
+
+# Annotation: max value highlight
+max_index = y.index(max(y))
+plt.annotate(
+    "Peak Sales",
+    xy=(x[max_index], y[max_index]),
+    xytext=(x[max_index]-1, y[max_index]+2),
+    arrowprops=dict(arrowstyle="->", color="red")
+)
+
+plt.title("Step Plot with Annotation")
 plt.xlabel("Days")
 plt.ylabel("Sales")
 plt.show()
