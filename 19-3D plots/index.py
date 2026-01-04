ae8a8cb951d1699747199fdd6dd8d08b887a09b2
CodeQueen-AI <codeq209@gmail.com>
2026-01-04 13:42:36 +0500

    update

--- 19-3D plots/index.py
+++ 19-3D plots/index.py
@@ -1,1 +1,11 @@
-# comments
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 13, 18, 20]
+sizes = [100, 300, 200, 500, 400]
+
+plt.scatter(x, y, s=sizes, alpha=0.6)
+plt.title("Bubble Chart")
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.show()
